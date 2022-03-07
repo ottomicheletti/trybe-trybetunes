a7a16a9346f38cb368ffc51185dbda5532f21b43
ottomicheletti <michelettiotto@gmail.com>
2022-03-07 17:38:56 -0300

    Requisito 04

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../../services/userAPI';
 import Loading from './Loading';
 
@@ -22,9 +23,34 @@ class Header extends Component {
     const { username, loading } = this.state;
     return (
       <header data-testid="header-component">
-        { loading
-          ? <Loading />
-          : <p data-testid="header-user-name">{ username }</p>}
+        <div className="user">
+          { loading
+            ? <Loading />
+            : <p data-testid="header-user-name">{ username }</p>}
+        </div>
+        <nav>
+          <Link
+            to="/search"
+            data-testid="link-to-search"
+            className="btn"
+          >
+            Busca
+          </Link>
+          <Link
+            to="/favorites"
+            data-testid="link-to-favorites"
+            className="btn"
+          >
+            Favoritos
+          </Link>
+          <Link
+            to="/profile"
+            data-testid="link-to-profile"
+            className="btn"
+          >
+            Perfil
+          </Link>
+        </nav>
       </header>
     );
   }
