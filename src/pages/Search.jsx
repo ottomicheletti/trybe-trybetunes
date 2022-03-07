c335de6132f48ea05eb98d399cd0c32c504dc192
ottomicheletti <michelettiotto@gmail.com>
2022-03-07 17:54:58 -0300

    Requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,44 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      btnDisabled: true,
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    const DOIS = 2;
+    if (value.length >= DOIS) {
+      this.setState({ btnDisabled: false });
+    } else {
+      this.setState({ btnDisabled: true });
+    }
+  }
+
   render() {
+    const { btnDisabled } = this.state;
     return (
       <div className="search" data-testid="page-search">
         PÁGINA DE BUSCA
         <Header />
+        <div className="query">
+          <input
+            type="text"
+            name=""
+            id=""
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            disabled={ btnDisabled }
+            type="submit"
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
