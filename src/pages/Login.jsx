4e1e220e5bed101ebb55c66b4cfcd9090d6db4c1
ottomicheletti <michelettiotto@gmail.com>
2022-03-07 15:51:42 -0300

    Requisito 02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,72 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from './components/Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      btnDisabled: true,
+      user: '',
+      loading: false,
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    const TRES = 3;
+    if (value.length >= TRES) {
+      this.setState({ btnDisabled: false });
+      this.setState({ user: value });
+    } else {
+      this.setState({ btnDisabled: true });
+      this.setState({ user: '' });
+    }
+  }
+
+  handleClick = async () => {
+    const { user } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name: `${user}` });
+    history.push('/search');
+  }
+
   render() {
+    const { btnDisabled, loading } = this.state;
     return (
       <div className="login" data-testid="page-login">
-        TELA DE LOGIN
+        {loading
+          ? <Loading />
+          : (
+            <div>
+              <input
+                type="text"
+                name=""
+                id=""
+                onChange={ this.handleChange }
+                data-testid="login-name-input"
+              />
+              <button
+                type="submit"
+                disabled={ btnDisabled }
+                onClick={ this.handleClick }
+                data-testid="login-submit-button"
+              >
+                Entrar
+              </button>
+            </div>
+
+          )}
+
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
 export default Login;
